76bcded5196b720e97fad4e6da849024f55f93b4
tobi <Tobias20.3@hotmail.de>
2022-10-04 18:05:52 +0200

    Disable blur and fix transparency for Firefox & Alacritty

--- nvim/lua/colorschemes-config/nord.lua
+++ nvim/lua/colorschemes-config/nord.lua
@@ -3,6 +3,7 @@
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
 vim.g.nord_italic = true
+vim.g.nord_disable_background = true
 
 -- vim.g.nord_enable_sidebar_background = true -- this kinda breaks the tree view
 
