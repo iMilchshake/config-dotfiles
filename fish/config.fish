a4b5d45fb57455bc38805924b2adcd72ba25d35a
tobi <Tobias20.3@hotmail.de>
2023-01-27 12:45:45 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     alias r="ranger"
     alias v="nvim"
     alias lg="lazygit"
+    export GTK_THEME="Nordic-darker"
+    pyenv init - | source
+    export PATH="$HOME/.cargo/bin:$PATH"
 end
