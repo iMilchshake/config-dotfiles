2ca5e9e449f2c5d461e96dfe27b20131a3f5dfd4
tobi <Tobias20.3@hotmail.de>
2023-05-08 11:13:28 +0200

    Add new aliases 'venv' and 'uni'

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     alias r="ranger"
     alias v="nvim"
     alias lg="lazygit"
+    alias venv="source ./venv/bin/activate.fish"
     alias night="brightnessctl set 1%;redshift -x;redshift -O 4800"
+    alias uni="ranger /home/tobi/Documents/UniversitySync/current_semester"
     export GTK_THEME="Nordic-darker"
     pyenv init - | source
     export PATH="$HOME/.cargo/bin:$PATH"
