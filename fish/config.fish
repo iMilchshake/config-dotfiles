46115f13fdac404286017f4e8e0be893fc230cfe
tobi <Tobias20.3@hotmail.de>
2023-11-05 18:47:11 +0100

    Remove GTK_THEME from fish config and add windoof alias
    GTK_THEME should be set inside /etc/environment !

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,8 @@ if status is-interactive
     alias venv="source ./venv/bin/activate.fish"
     alias night="brightnessctl set 1%;redshift -x;redshift -O 4800"
     alias uni="ranger /home/tobi/Documents/UniversitySync/current_semester"
-    export GTK_THEME="Nordic-darker"
+    alias windoof="sudo grub-reboot 2;reboot"
+    # export GTK_THEME="Arc-Dark"
     pyenv init - | source
     export PATH="$HOME/.cargo/bin:$PATH"
 end
