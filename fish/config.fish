c368a885c55c141aa876c19f12bcb86daa38d92a
tobi <Tobias20.3@hotmail.de>
2023-11-06 15:44:59 +0100

    Add qs alias

--- fish/config.fish
+++ fish/config.fish
@@ -9,4 +9,6 @@ if status is-interactive
     alias settings="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
     pyenv init - | source
     export PATH="$HOME/.cargo/bin:$PATH"
+    alias qs="nvim /home/tobi/data/quickshare/qs.txt"
 end
+
