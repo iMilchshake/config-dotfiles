fa0f8ad4f30bf23e21a6423fdf526be787987c1f
tobi <Tobias20.3@hotmail.de>
2023-11-06 15:36:15 +0100

    Update uni alias to match global folder structure

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     alias lg="lazygit"
     alias venv="source ./venv/bin/activate.fish"
     alias night="brightnessctl set 1%;redshift -x;redshift -O 4800"
-    alias uni="ranger /home/tobi/Documents/UniversitySync/current_semester"
+    alias uni="ranger /home/tobi/docs/UniversitySync/current_semester"
     alias windoof="sudo grub-reboot 2;reboot"
     alias settings="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center"
     pyenv init - | source
