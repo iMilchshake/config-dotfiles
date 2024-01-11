b88f1054a2063a9b20f8e53df47340cd5a837cb2
tobi <Tobias20.3@hotmail.de>
2024-01-12 00:40:28 +0100

    Simplify polybar launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
-
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -c ~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Polybar launched..."
+polybar --reload main 
