360f640dfd040df21e7809eb20d1436ae345f36d
tobi <Tobias20.3@hotmail.de>
2024-05-05 16:29:15 +0200

    Add aerial to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true;
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
