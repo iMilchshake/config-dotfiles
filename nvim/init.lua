7161d5b217c906cb837ef53633f5aac0fa9a1f22
tobi <Tobias20.3@hotmail.de>
2024-11-12 12:21:49 +0100

    Update nvim and add temporary rust-analyzer fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -552,22 +552,32 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+require('which-key').add({
+  { '<leader>c',  group = '[C]ode' },
+  { '<leader>d',  group = '[D]ocument' },
+  { '<leader>g',  group = '[G]it' },
+  { '<leader>h',  group = 'Git [H]unk' },
+  { '<leader>r',  group = '[R]ename' },
+  { '<leader>s',  group = '[S]earch' },
+  { '<leader>t',  group = '[T]oggle' },
+  { '<leader>w',  group = '[W]orkspace' },
+
+  -- Adding hidden key bindings
+  { '<leader>c_', hidden = true },
+  { '<leader>d_', hidden = true },
+  { '<leader>g_', hidden = true },
+  { '<leader>h_', hidden = true },
+  { '<leader>r_', hidden = true },
+  { '<leader>s_', hidden = true },
+  { '<leader>t_', hidden = true },
+  { '<leader>w_', hidden = true },
+})
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+require('which-key').add({
+  { '<leader>',  group = 'visual <leader>', mode = 'v' },
+  { '<leader>h', desc = 'Git [H]unk',       mode = 'v' },
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -681,3 +691,15 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+-- FIXME: TEMPORARY WORKAROUND -> REVERT WHEN FIXED
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
