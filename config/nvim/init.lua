63165e1dcebdbe901167237e028493bde674ba77
tobi <Tobias20.3@hotmail.de>
2025-05-18 21:49:41 +0200

    Pin mason to latest v1 release

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -84,12 +84,12 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'williamboman/mason.nvim',           version = '1.*', config = true },
+      { 'williamboman/mason-lspconfig.nvim', tag = 'v1.32.0' },
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      { 'j-hui/fidget.nvim',                 opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
